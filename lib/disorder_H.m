725ea22b26664962727a9973b0cb8f5b2b213396
GroundhogState <jake.ross271@gmail.com>
2018-11-24 19:01:05 +1100

    Finished tidying workflow. Rescaling function now more sensible.

--- lib/disorder_H.m
+++ lib/disorder_H.m
@@ -1,11 +1,11 @@
-function [H, h_list] = disorder_H(L,W)
-    h_list = W*(-1+2*rand(L,1));
+function [H, h_list] = disorder_H(config)
+    h_list = config.gen.W*(-1+2*rand(L,1));
 % <<<<<<< HEAD
-    H_onsite = 0.5*gen_onsite(L,h_list);
+    H_onsite = 0.5*gen_onsite(config.gen.L,h_list);
 % =======
 %     H_onsite = gen_onsite(L,0.5*h_list);
 % >>>>>>> 501355829750fddf03df7a6f16452b7f6c42956f
     bc = 'periodic';
-    H_int =0.25* get_interaction(L,bc);
+    H_int =0.25* get_interaction(config.gen.L,config.gen.bc);
     H = H_onsite + H_int;
 end
--- lib/vec_to_graph.m
+++ lib/vec_to_graph.m
@@ -1,7 +1,8 @@
 function graph_data = vec_to_graph(vs)
 
-    graph_data.A_list = cell(size(vs,2),1);
+    graph_data = cell(size(vs,2),1);
     for k=1:size(vs,2)
+        
         v = vs(:,k)/norm(vs(:,k)); %just in case
         L = log2(numel(v));
         rho = v.*v';
@@ -10,30 +11,30 @@ function graph_data = vec_to_graph(vs)
         A = zeros(L);
         A_temp = zeros(L);
         systems = 1:L;
-        for ii=1:L %sadly do need to loop over this twice - otherwise A is generated incorrectly. Would be great fun to come up with a more efficient method.
-            trace_sys = systems;
-            trace_sys(ii) = [];
-%             red_ii = TrX(rho, trace_sys,dims);
-%             Entropy(red_ii);
-            ent_list(ii) = Entropy(TrX(rho, trace_sys,dims));
-        end
-        for ii=1:L
+
+        for ii=1:L 
             for jj = ii+1:L
                trace_pair = systems;
                trace_pair(ii) =[];
                trace_pair(jj-1) = [];
-               A_temp(ii,jj) =  Entropy(TrX(rho,trace_pair,dims));
-%                fprintf('S_AB %f' ,A(ii,jj))
+               rho_red = TrX(rho,trace_pair,dims);
+               A_temp(ii,jj) =  Entropy(rho_red);
+            end
+            if ii==L
+                ent_list(ii) = Entropy(TrX(rho_red,1,[2,2]));
+            else
+                ent_list(ii) = Entropy(TrX(rho_red,2,[2,2]));
             end
         end
-        A_temp = abs((A_temp + A_temp'));
+
+        A_temp = abs(A_temp + A_temp');
         for ii=1:L
             for jj=1:L
             A(ii,jj) = abs(ent_list(ii)) + abs(ent_list(jj)) - A_temp(ii,jj);  
             end
         end
 
-        graph_data.A_list{k} = A; %Aleph - has 2*on-site entropy on diagonal. Can recover G (zero diag) easy)
+        graph_data{k} = A; %Aleph - has 2*on-site entropy on diagonal. Can recover G (zero diag) easy)
     end
 
 
